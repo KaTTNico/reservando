abc17a427edc4bdd2fc12b6e1b5d5a94f5152bbe
Nicolas <aguirrecampi.nicolas@gmail.com>
2019-04-11 22:10:48 -0300

    SE REFACTORIZA Y SE TESTEA, GUIA 1 Y GUIA 2 COMLETADO.

--- recursos-reservando/js/restaurant.js
+++ recursos-reservando/js/restaurant.js
@@ -1,39 +1,27 @@
 var Restaurant = function(id, nombre, rubro, ubicacion, horarios, imagen, calificaciones) {
-    this.id = id;
-    this.nombre = nombre;
-    this.rubro = rubro;
-    this.ubicacion = ubicacion;
-    this.horarios = horarios;
-    this.imagen = imagen;
-    this.calificaciones = calificaciones;
+  this.id = id;
+  this.nombre = nombre;
+  this.rubro = rubro;
+  this.ubicacion = ubicacion;
+  this.horarios = horarios;
+  this.imagen = imagen;
+  this.calificaciones = calificaciones;
 }
 
 Restaurant.prototype.reservarHorario = function(horarioReservado) {
-    for (var i = 0; i < this.horarios.length; i++) {
-        if (this.horarios[i] === horarioReservado) {
-            this.horarios.splice(i, 1);
-            return;
-        }
-    }
+  this.horarios = this.horarios.filter(horario => horario != horarioReservado)
 }
 
 Restaurant.prototype.calificar = function(nuevaCalificacion) {
-    if (Number.isInteger(nuevaCalificacion) && nuevaCalificacion > 0 && nuevaCalificacion < 10) {
-        this.calificaciones.push(nuevaCalificacion);
-    }
+  if (Number.isInteger(nuevaCalificacion) && nuevaCalificacion > 0 && nuevaCalificacion < 10) {
+    this.calificaciones.push(nuevaCalificacion);
+  }
 }
+
+var sumatoria = (numeros) => numeros.reduce((total, numero) => total + numero)
+
+var promedio = (numeros) => (numeros.length != 0) ? sumatoria(numeros) / numeros.length : 0
 
 Restaurant.prototype.obtenerPuntuacion = function() {
-    if (this.calificaciones.length === 0) {
-        return 0;
-    } else {
-        var sumatoria = 0;
-        for (var i = 0; i < this.calificaciones.length; i++) {
-            sumatoria += this.calificaciones[i]
-        }
-        var promedio = sumatoria / this.calificaciones.length;
-        return Math.round(promedio * 10) / 10;
-    }
-
+  return promedio(this.calificaciones);
 }
-
---
+++ recursos-reservando/js/tests.js
@@ -0,0 +1,127 @@
+var expect = chai.expect
+
+/*TESTS DE LA CLASE RESTAURANT*/
+describe('reservar un horario', function() {
+  //este es un restaurant robado del archivo listado.js
+  let restaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5])
+
+  it('Cuando se reserva un horario de un restaurant, el horario correspondiente se elimina del arreglo.', function() {
+    //reservar horario
+    restaurant.reservarHorario("13:00")
+
+    expect(restaurant.horarios).not.have.members(['13:00'])
+  })
+
+  it('Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.', function() {
+    //obtener la cantidad de horarios actual.
+    let cantidadHorarios = restaurant.horarios.lenght
+    //se reserva un horario que el restaurant no posee.
+    restaurant.reservarHorario("14:00")
+
+    expect(restaurant.horarios.lenght).to.be.equal(cantidadHorarios)
+  })
+
+  it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.', function() {
+    //obtener la cantidad de horarios actual.
+    let cantidadHorarios = restaurant.horarios.lenght
+    //se llama a la funcion reservarHorario pero no se le pasa ningun parametro
+    restaurant.reservarHorario()
+
+    expect(restaurant.horarios.lenght).to.be.equal(cantidadHorarios)
+  })
+})
+
+describe('Obtener una puntuacion', function() {
+  it('Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.', function() {
+    //este es un restaurant robado del archivo listado.js
+    let restaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5])
+    //obtener puntuacion
+    let puntuacion = restaurant.obtenerPuntuacion();
+
+    /*[6, 7, 9, 10, 5] el resultado del promedio de estas puntuaciones es 7.4*/
+    expect(puntuacion).to.be.equal(7.4)
+  })
+
+  it('Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.', function() {
+    //este es un restaurant robado del achivo listado.js pero sin calificar
+    let restaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [])
+    //obtener puntuacione
+    let puntuacion = restaurant.obtenerPuntuacion();
+
+    expect(puntuacion).to.be.equal(0)
+  })
+})
+
+describe('Agregar una nueva calificacion', function() {
+  //este es un restaurant robado del archivo listado.js
+  let restaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5])
+
+  it('Dado un restaurant, al darle una calificacion, esta debe formar parte de las calificaciones del mismo.', function() {
+    //calificar con una calificacion que no tenia
+    restaurant.calificar(8)
+
+    expect(restaurant.calificaciones).to.include.members([8])
+  })
+
+  it('Dado un restaurant, al llamar al metodo calificar y no pasarle nada no deberian cambiar las calificaciones del mismo', function() {
+    //get calificaciones
+    let calificaciones = restaurant.calificaciones
+
+    //calificar vacio
+    restaurant.calificar()
+
+    expect(restaurant.calificaciones).to.have.equal(calificaciones)
+  })
+})
+
+/*TESTS DE LA CLASE LISTADO*/
+
+describe('Buscar un restaurant', function() {
+  it('Dada una lista de restaurantes, al buscar un restaurant existente debe devolverme el mismo.', function() {
+    expect(listado.restaurantes.find(restaurant => restaurant.id == 1)).to.be.eql(listado.buscarRestaurante(1))
+  })
+
+  it('Dada una lista de restaurantes, al buscar un id que no existe no me debe devolver ningun restaurant.', function() {
+    expect(listado.buscarRestaurante(0)).to.be.equal('No se ha encontrado ningún restaurant')
+  })
+
+  it('Dada una lista de restaurantes, al no pasarle nada por parametro a buscarRestaurante no me debe devolver ningun restaurant.', function() {
+    expect(listado.buscarRestaurante()).to.be.equal('No se ha encontrado ningún restaurant')
+  })
+
+})
+
+describe('Obtener restaurantes', function() {
+  var filtroRubro = ""
+  var filtroCiudad = ""
+  var filtroHorario = ""
+
+  afterEach(function() {
+    if (filtroRubro !== null && filtroCiudad !== null && filtroHorario !== null) {
+      expect(listado.restaurantes.filter(restaurant => (
+        restaurant.rubro == filtroRubro &&
+        restaurant.ubicacion == filtroCiudad &&
+        (restaurant.horarios.some(horario => horario == filtroHorario))
+      ))).to.be.eql(listado.obtenerRestaurantes(filtroRubro, filtroCiudad, filtroHorario))
+    } else
+      expect(listado.restaurantes).to.be.eql(listado.obtenerRestaurantes(filtroRubro, filtroCiudad, filtroHorario))
+  });
+
+  it('Dada una lista de restaurantes, al filtrar los restaurantes deberia devolverme todos los restaurantes.', function() {
+    filtroRubro = "Desayuno"
+    filtroCiudad = "Nueva York"
+    filtroHorario = "15:00"
+  })
+
+  it('Dada una lista de restaurantes, al filtrar los restaurantes con los filtros vacios a menos que existan restaurantes con estas atributos vacios (en este caso no hay)', function() {
+    filtroRubro = ""
+    filtroCiudad = ""
+    filtroHorario = ""
+  })
+
+  it('Dada una lista de restaurantes, al filtrar los restaurantes con los filtros null me debe devolver todos los restaurantes.', function() {
+    filtroRubro = null
+    filtroCiudad = null
+    filtroHorario = null
+  })
+})
